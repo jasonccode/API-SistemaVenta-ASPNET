7885635951ec4939da9c2f4862697402f78adf41
Jason C <grousemusic@gmail.com>
2023-12-10 16:10:15 -0500

    Creando IGenericRepository

---
+++ SistemaVenta.DAL/Repositorios/Contrato/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace APISistemaVenta.SistemaVenta.DAL.Repositorios.Contrato
+{
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro);
+        Task<TModel> Crear(TModel modelo);
+        Task<bool> Editar(TModel modelo);
+        Task<bool> Eliminar(TModel modelo);
+        Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null);
+    }
+}
