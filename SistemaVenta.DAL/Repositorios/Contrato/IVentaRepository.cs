be70ff43b60cf4d928f8bc15949e4440ed85af45
Jason C <grousemusic@gmail.com>
2023-12-10 17:00:46 -0500

    Metodos GenericRepository

--- SistemaVenta.DAL/Repositorios/Contrato/IVentaRepository.cs
+++ SistemaVenta.DAL/Repositorios/Contrato/IVentaRepository.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using APISistemaVenta.Models;
 
 namespace APISistemaVenta.SistemaVenta.DAL.Repositorios.Contrato
