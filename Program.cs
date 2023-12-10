fb0c5db9d49ca98b56be4099fa7071f591ec0991
Jason C <grousemusic@gmail.com>
2023-12-09 20:04:31 -0500

    Creando conexion posgrest y dotnet ef dbcontext scaffold

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using APISistemaVenta.SistemaVenta.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Conexion postgres
+builder.Services.AddNpgsql<DbVentaContext>(builder.Configuration.GetConnectionString("Postgres"));
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 
 var app = builder.Build();
 
---
+++ SistemaVenta.Model/DbventaContext.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APISistemaVenta.Models;
+
+public partial class DbventaContext : DbContext
+{
+    public DbventaContext()
+    {
+    }
+
+    public DbventaContext(DbContextOptions<DbventaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categorium> Categoria { get; set; }
+
+    public virtual DbSet<DetalleVentum> DetalleVenta { get; set; }
+
+    public virtual DbSet<Menu> Menus { get; set; }
+
+    public virtual DbSet<MenuRol> MenuRols { get; set; }
+
+    public virtual DbSet<NumeroDocumento> NumeroDocumentos { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Rol> Rols { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    public virtual DbSet<Ventum> Venta { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=dbventa;Username=postgres;Password=1234");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categorium>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("Categoria_pkey");
+
+            entity.Property(e => e.IdCategoria)
+                .HasDefaultValueSql("nextval('categoria_idcategoria_seq'::regclass)")
+                .HasColumnName("idCategoria");
+            entity.Property(e => e.EsActivo)
+                .HasDefaultValueSql("true")
+                .HasColumnName("esActivo");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<DetalleVentum>(entity =>
+        {
+            entity.HasKey(e => e.IdDetalleVenta).HasName("DetalleVenta_pkey");
+
+            entity.Property(e => e.IdDetalleVenta)
+                .HasDefaultValueSql("nextval('detalleventa_iddetalleventa_seq'::regclass)")
+                .HasColumnName("idDetalleVenta");
+            entity.Property(e => e.Cantidad).HasColumnName("cantidad");
+            entity.Property(e => e.IdProducto).HasColumnName("idProducto");
+            entity.Property(e => e.IdVenta).HasColumnName("idVenta");
+            entity.Property(e => e.Precio)
+                .HasPrecision(10, 2)
+                .HasColumnName("precio");
+            entity.Property(e => e.Total)
+                .HasPrecision(10, 2)
+                .HasColumnName("total");
+
+            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.DetalleVenta)
+                .HasForeignKey(d => d.IdProducto)
+                .HasConstraintName("detalleventa_idproducto_fkey");
+
+            entity.HasOne(d => d.IdVentaNavigation).WithMany(p => p.DetalleVenta)
+                .HasForeignKey(d => d.IdVenta)
+                .HasConstraintName("detalleventa_idventa_fkey");
+        });
+
+        modelBuilder.Entity<Menu>(entity =>
+        {
+            entity.HasKey(e => e.IdMenu).HasName("Menu_pkey");
+
+            entity.ToTable("Menu");
+
+            entity.Property(e => e.IdMenu)
+                .HasDefaultValueSql("nextval('menu_idmenu_seq'::regclass)")
+                .HasColumnName("idMenu");
+            entity.Property(e => e.Icono)
+                .HasMaxLength(50)
+                .HasColumnName("icono");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Url)
+                .HasMaxLength(50)
+                .HasColumnName("url");
+        });
+
+        modelBuilder.Entity<MenuRol>(entity =>
+        {
+            entity.HasKey(e => e.IdMenuRol).HasName("MenuRol_pkey");
+
+            entity.ToTable("MenuRol");
+
+            entity.Property(e => e.IdMenuRol)
+                .HasDefaultValueSql("nextval('menurol_idmenurol_seq'::regclass)")
+                .HasColumnName("idMenuRol");
+            entity.Property(e => e.IdMenu).HasColumnName("idMenu");
+            entity.Property(e => e.IdRol).HasColumnName("idRol");
+
+            entity.HasOne(d => d.IdMenuNavigation).WithMany(p => p.MenuRols)
+                .HasForeignKey(d => d.IdMenu)
+                .HasConstraintName("menurol_idmenu_fkey");
+
+            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.MenuRols)
+                .HasForeignKey(d => d.IdRol)
+                .HasConstraintName("menurol_idrol_fkey");
+        });
+
+        modelBuilder.Entity<NumeroDocumento>(entity =>
+        {
+            entity.HasKey(e => e.IdNumeroDocumento).HasName("NumeroDocumento_pkey");
+
+            entity.ToTable("NumeroDocumento");
+
+            entity.Property(e => e.IdNumeroDocumento)
+                .HasDefaultValueSql("nextval('numerodocumento_idnumerodocumento_seq'::regclass)")
+                .HasColumnName("idNumeroDocumento");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.UltimoNumero).HasColumnName("ultimo_Numero");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("Producto_pkey");
+
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.IdProducto)
+                .HasDefaultValueSql("nextval('producto_idproducto_seq'::regclass)")
+                .HasColumnName("idProducto");
+            entity.Property(e => e.EsActivo)
+                .HasDefaultValueSql("true")
+                .HasColumnName("esActivo");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.IdCategoria).HasColumnName("idCategoria");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Precio)
+                .HasPrecision(10, 2)
+                .HasColumnName("precio");
+            entity.Property(e => e.Stock).HasColumnName("stock");
+
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("producto_idcategoria_fkey");
+        });
+
+        modelBuilder.Entity<Rol>(entity =>
+        {
+            entity.HasKey(e => e.IdRol).HasName("Rol_pkey");
+
+            entity.ToTable("Rol");
+
+            entity.Property(e => e.IdRol)
+                .HasDefaultValueSql("nextval('rol_idrol_seq'::regclass)")
+                .HasColumnName("idRol");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("Usuario_pkey");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.IdUsuario)
+                .HasDefaultValueSql("nextval('usuario_idusuario_seq'::regclass)")
+                .HasColumnName("idUsuario");
+            entity.Property(e => e.Clave)
+                .HasMaxLength(40)
+                .HasColumnName("clave");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(40)
+                .HasColumnName("correo");
+            entity.Property(e => e.EsActivo)
+                .HasDefaultValueSql("true")
+                .HasColumnName("esActivo");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.IdRol).HasColumnName("idRol");
+            entity.Property(e => e.NombreCompleto)
+                .HasMaxLength(100)
+                .HasColumnName("nombreCompleto");
+
+            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.Usuarios)
+                .HasForeignKey(d => d.IdRol)
+                .HasConstraintName("usuario_idrol_fkey");
+        });
+
+        modelBuilder.Entity<Ventum>(entity =>
+        {
+            entity.HasKey(e => e.IdVenta).HasName("Venta_pkey");
+
+            entity.Property(e => e.IdVenta)
+                .HasDefaultValueSql("nextval('venta_idventa_seq'::regclass)")
+                .HasColumnName("idVenta");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("fechaRegistro");
+            entity.Property(e => e.NumeroDocumento)
+                .HasMaxLength(40)
+                .HasColumnName("numeroDocumento");
+            entity.Property(e => e.TipoPago)
+                .HasMaxLength(50)
+                .HasColumnName("tipoPago");
+            entity.Property(e => e.Total)
+                .HasPrecision(10, 2)
+                .HasColumnName("total");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
