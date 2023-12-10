dcec8c329fa3e0c742a985f334f9bf0e6767c2fd
Jason C <grousemusic@gmail.com>
2023-12-09 20:51:22 -0500

    ajuste dbcontext pasa dao

--- SistemaVenta.DAL/DbContext/DbventaContext.cs
+++ SistemaVenta.DAL/DbContext/DbventaContext.cs
@@ -33,11 +33,6 @@ public partial class DbventaContext : DbContext
 
     public virtual DbSet<Venta> Venta { get; set; }
 
-   // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-
-// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-
-    //         => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=dbventa;Username=postgres;Password=1234");
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
